d91581308d963e2dde6933ae273c8bbb13338a24
Kzrysztof Hintz <khintz222@gmail.com>
2019-07-18 13:18:58 +0200

    merge repos and view models

--- app/src/main/java/com/apator/map/MainActivity.kt
+++ app/src/main/java/com/apator/map/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.apator.map
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import com.apator.map.viewmodel.SolarListViewModel
 import com.apator.map.viewmodel.SolarViewModel
-import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
-    private val solarViewModel: SolarListViewModel = SolarListViewModel()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
