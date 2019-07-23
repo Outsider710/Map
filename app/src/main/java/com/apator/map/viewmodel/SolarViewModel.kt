aecfc5d397bf16d55db7f2c262f93463adc7b36a
Kzrysztof Hintz <khintz222@gmail.com>
2019-07-23 10:36:22 +0200

    refactor of SolarApi

--- app/src/main/java/com/apator/map/viewmodel/SolarViewModel.kt
+++ app/src/main/java/com/apator/map/viewmodel/SolarViewModel.kt
@@ -53,30 +53,14 @@ class SolarViewModel(application: Application) : AndroidViewModel(application) {
     fun fetchSolar(
         format: String = "json",
         api_key: String = "5Gibh08OmtfPAZXEF4qhLJc7ckXxBPL8PBlst9ws",
-        lat: Double = 0.0,
-        lon: Double = 0.0,
-        file_id: String = "2-566510-EPW-CSWD-epw",
-        system_capacity: Double = 4.0,
-        azimuth: Double = 180.0,
-        tilt: Double = 40.0,
-        array_type: Int = 1,
-        module_type: Int = 1,
-        losses: Double = 10.0
+        file_id: String = "2-566510-EPW-CSWD-epw"
     ) {
         scope.launch {
 
             val solar = repository.getDetails(
                 format,
                 api_key,
-                lat,
-                lon,
-                file_id,
-                system_capacity,
-                azimuth,
-                tilt,
-                array_type,
-                module_type,
-                losses
+                file_id
             )
             solarDetailsLiveData.postValue(solar)
         }
